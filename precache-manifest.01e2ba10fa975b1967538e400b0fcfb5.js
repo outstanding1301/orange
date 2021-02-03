35d60604c4990e62ca30a6e06a13d4e6c5470d23
outstandingboy <outstandingboy@kakao.com>
2021-02-03 23:56:59 +0900

    Updates

---
+++ precache-manifest.01e2ba10fa975b1967538e400b0fcfb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b55a6257ef0cde7e3bfdfe28ced520d",
+    "url": "/orange/index.html"
+  },
+  {
+    "revision": "f5b29f142f647e7641f6",
+    "url": "/orange/static/css/main.231c0fa7.chunk.css"
+  },
+  {
+    "revision": "c068913fd04e7e0a11c4",
+    "url": "/orange/static/js/2.1b666dee.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/orange/static/js/2.1b666dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5b29f142f647e7641f6",
+    "url": "/orange/static/js/main.070b0fdf.chunk.js"
+  },
+  {
+    "revision": "9b8688fe9dd5059ba4c0",
+    "url": "/orange/static/js/runtime-main.1862adad.js"
+  }
+]);
